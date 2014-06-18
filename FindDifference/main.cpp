6ba806edce5fc1fb5b8b9b5827ea755ab7074e66
NicoBarbaros <barbarosnicolae93@gmail.com>
2014-06-18 17:37:28 +0300

    Bla

--- FindDifference/main.cpp
+++ FindDifference/main.cpp
@@ -15,7 +15,7 @@ static int iSysHeight;
 HINSTANCE hInst;
 
 char* Images[3] = {"Cat","Guffy","Lupu"};
-
+int random = -1;
 
 /*  Declare Windows procedure  */
 LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
@@ -111,11 +111,8 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
     PAINTSTRUCT Ps;
     HDC hdc = GetDC(hwnd);
     HBRUSH hbrush;
-    RECT cat;
-    RECT guffy;
-    RECT wolf;
+    static RECT diff[30];
 
-    int random = GetRandom(3);
     printf("%d",random);
     char* current_img = Images[random];
 
@@ -134,76 +131,17 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
 	xMouse = GET_X_LPARAM(lParam);
 	yMouse = GET_Y_LPARAM(lParam);
 
-    //Array of Rectangles
-    RECT arr[30];
-
     //Coordinates of the differences
-    RECT cat1 = {508,122,569,165};
-    RECT cat2 = {511,161,568,180};  //sprincene
-    RECT cat3 = {540, 197, 560, 217 };//ochi
-    RECT cat4 = {583, 228, 614, 245};//mustata dreapta
-    RECT cat5 = {645, 328, 685, 348};//coada
-    RECT cat6 = {586, 292, 630, 327};//fluture
-    RECT cat7 = {498, 359, 526, 388};//hand
-    RECT cat8 = {520, 289, 557, 305};//piept
-    RECT cat9 = {540, 390, 550, 400 };//flowerbottom
-    RECT cat10 = {450, 310, 470, 330};//left
-
-    RECT guffy1 = {508,122,569,165};
-    RECT guffy2 = {511,161,568,180};  //sprincene
-    RECT guffy3 = {540, 197, 560, 217 };//ochi
-    RECT guffy4 = {583, 228, 614, 245};//mustata dreapta
-    RECT guffy5 = {645, 328, 685, 348};//coada
-    RECT guffy6 = {586, 292, 630, 327};//fluture
-    RECT guffy7 = {498, 359, 526, 388};//hand
-    RECT guffy8 = {520, 289, 557, 305};//piept
-    RECT guffy9 = {540, 390, 550, 400 };//flowerbottom
-    RECT guffy10 = {450, 310, 470, 330};//left
-
-    RECT wolf1 = {508,122,569,165};
-    RECT wolf2 = {511,161,568,180};  //sprincene
-    RECT wolf3 = {540, 197, 560, 217 };//ochi
-    RECT wolf4 = {583, 228, 614, 245};//mustata dreapta
-    RECT wolf5 = {645, 328, 685, 348};//coada
-    RECT wolf6 = {586, 292, 630, 327};//fluture
-    RECT wolf7 = {498, 359, 526, 388};//hand
-    RECT wolf8 = {520, 289, 557, 305};//piept
-    RECT wolf9 = {540, 390, 550, 400 };//flowerbottom
-    RECT wolf10 = {450, 310, 470, 330};//left
-
-    //Assigning the values of cat to it's possition in array
-    arr[0] = cat1;
-    arr[1] = cat2;
-    arr[2] = cat3;
-    arr[3] = cat4;
-    arr[4] = cat5;
-    arr[5] = cat6;
-    arr[6] = cat7;
-    arr[7] = cat8;
-    arr[8] = cat9;
-    arr[9] = cat10;
-
-    arr[10] = guffy1;
-    arr[11] = guffy2;
-    arr[12] = guffy3;
-    arr[13] = guffy4;
-    arr[14] = guffy5;
-    arr[15] = guffy6;
-    arr[16] = guffy7;
-    arr[17] = guffy8;
-    arr[18] = guffy9;
-    arr[19] = guffy10;
-
-    arr[20] = wolf1;
-    arr[21] = wolf2;
-    arr[22] = wolf3;
-    arr[23] = wolf4;
-    arr[24] = wolf5;
-    arr[25] = wolf6;
-    arr[26] = wolf7;
-    arr[27] = wolf8;
-    arr[28] = wolf9;
-    arr[29] = wolf10;
+    diff[0] = {508,122,569,165};
+    diff[1] = {511,161,568,180};  //sprincene
+    diff[2] = {540, 197, 560, 217 };//ochi
+    diff[3] = {583, 228, 614, 245};//mustata dreapta
+    diff[4] = {645, 328, 685, 348};//coada
+    diff[5] = {586, 292, 630, 327};//fluture
+    diff[6] = {498, 359, 526, 388};//hand
+    diff[7] = {520, 289, 557, 305};//piept
+    diff[8] = {540, 390, 550, 400 };//flowerbottom
+    diff[9] = {450, 310, 470, 330};//left
 
     switch (message)                  /* handle the messages */
     {
@@ -231,7 +169,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
                 {
                     InvalidateRect(hwnd, &area, FALSE);
                     InvalidateRect(hwnd, &area, TRUE);
-
+                    random = GetRandom(3);
                     for (int i=0; i<11; i++)
                     {
                         CheckDlgButton(hwnd, IDs[i], BST_UNCHECKED);
@@ -258,12 +196,12 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
     //Work with LButton
     case WM_LBUTTONDOWN:
         {
-            for (int i = 0; i < sizeof(arr)/sizeof(RECT); i++)
+            for (int i = 0; i < sizeof(diff)/sizeof(RECT); i++)
             {
-                if(arr[i].left < xMouse && xMouse < arr[i].right && arr[i].top < yMouse && yMouse < arr[i].bottom )
+                if(diff[i].left < xMouse && xMouse < diff[i].right && diff[i].top < yMouse && yMouse < diff[i].bottom )
                 {
                     PlaySound("Level.wav", NULL, SND_ASYNC);
-                    DrawEdge(hdc, &arr[i], BDR_RAISEDOUTER | BDR_SUNKENINNER, BF_RECT);
+                    DrawEdge(hdc, &diff[i], BDR_RAISEDOUTER | BDR_SUNKENINNER, BF_RECT);
                     CheckDlgButton(hwnd, IDs[i], BST_CHECKED);
                     MessageBoxA(NULL,"You found it! Good Job", "Congrats", MB_OK | MB_ICONINFORMATION);
 
@@ -301,7 +239,7 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             BitBlt(hdc, 400, 80, bitmapCat2.bmWidth, bitmapCat2.bmHeight, hdcCat2, 0, 0, SRCCOPY);
             DeleteObject(hdcCat2);
 
-            GetClientRect(hwnd, &cat);  // retrieves the coordinates of a window's client area
+            //GetClientRect(hwnd, &cat);  // retrieves the coordinates of a window's client area
 
             // create the title
             font_forte   = CreateFont(30, 27.5, 0, 0, FW_DONTCARE, false, false, false,
